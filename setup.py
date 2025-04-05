264cb5c7efb27d141e0196366a80f40a248627fd
Sourav <souravraj664@gmail.com>
2025-04-05 17:55:26 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project is about predicting no. of pickup in specific region',
+    author='hunter',
+    license='',
+)
